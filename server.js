ae0b693fe500aaffe0ef47fd97d6afde7dbabe33
Sarawuth <sarawuthwongsanga@gmail.com>
2022-02-09 15:06:09 +0700

    Connect with MONGODB

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const dotenv = require('dotenv');
+//Edit 2
+const connectDB = require('./config/db');
+const res = require('express/lib/response');
+
+const hospitals = require ('./routes/hospitals');
+//load env var
+dotenv.config({path:'./config/config.env'});
+//Edit 2
+connectDB();
+
+const app = express();
+//Body parser
+app.use(express.json());
+
+app.use('/api/v1/hospitals', hospitals);
+
+const PORT=process.env.PORT || 5000;
+const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+//Handle unhandled promise rejections
+process.on('unhandledRejection',(err,promise)=>{
+    console.log(`Error: ${err.message}`);
+    //Close server & exit process
+    server.close(()=>process.exit(1));
+})
